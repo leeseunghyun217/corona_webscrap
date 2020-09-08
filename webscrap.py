3581d662a11c250be9d02bbf667a2acf0516ce5e
Joe Lee <64088873+leeseunghyun217@users.noreply.github.com>
2020-09-08 22:33:00 +0900

    Update webscrap.py

--- webscrap.py
+++ webscrap.py
@@ -10,6 +10,7 @@ r = session.get('https://search.naver.com/search.naver?sm=top_sug.pre&fbm=1&acr=
 
 info_blocks = r.html.find('div.status_info',first=True)
 info_block = info_blocks.find('li')
+
 lst = []
 
 for info in info_block:
@@ -17,62 +18,69 @@ for info in info_block:
     daily_num = info.find('em',first=True).text
     lst.append(total_num);lst.append(daily_num)
 
-
+string =  'Total cases : {lst[0]} \
+           Daily cases :{lst[1]} \
+           Total testing : {lst[2]} \
+           Daily testing : {lst[3]} \
+           Total recovered : {lst[4]} \
+           Daily recovered: {lst[5]} \
+           Total deaths : {lst[6]} \
+           Daily deaths : {lst[7]} '
+    
 EMAIL_ADD = os.environ.get('EMAIL_ID')
 EMAIL_PASS = os.environ.get('EMAIL_PASS')
 
-string =  '총 확진자 : {lst[0]}명 \
-            오늘 확진자 :{lst[1]}명\
-            총 검사진행 : {lst[2]}명\
-            오늘 검사진행 : {lst[3]}명\
-            총 격리해제 : {lst[4]}명\
-            오늘 격리해제 : {lst[5]}명\
-            총 사망자 : {lst[6]}명\
-            오늘 사망자 : {lst[7]}명'
-
 msg = EmailMessage()
-msg['Subject'] = '코로나 알람!'
-msg['From'] = EMAIL_ADD
-msg['To'] = EMAIL_ADD
-msg.set_content(string)
+msg['Subject'] = 'Corona Alert!' # enter subject of mail 
+msg['From'] = EMAIL_ADD # enter sender email
+msg['To'] = EMAIL_ADD # enter receiver email 
 
+# Sending mail to multiple users 
+#---------------------------------------------------
+# contacts = ['user1@mail.com','user2@mail.com'...]
 
+# Method 1 : 
+# msg['To'] = contacts 
+
+# Method 2 ( Method 1 sometimes does not work / msg['To'] reads comma seperated strings ) : 
+# msg['To'] = ','.join(contacts) 
+
+msg.set_content(string) # text if html is not read by email 
 
 msg.add_alternative('''\
 <!doctype html>
 <html>
+<head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Corona Virus Numbers</title>
+</head>
 <body>
-    <p>총 확진자 : {lst[0]}명</p>
-    <p>오늘 확진자 :{lst[1]}명</p>
-    <p>총 검사진행 : {lst[2]}명</p>
-    <p오늘 검사진행 : {lst[3]}명</p>
+    <p>Total cases : {lst[0]}</p>
+    <p>Daily cases : {lst[1]}</p>
+    <p>Total testing : {lst[2]}</p>
+    <pDaily testing : {lst[3]}</p>
 
-    <p>총 격리해제 : {lst[4]}명</p>
-    <p>오늘 격리해제 : {lst[5]}명</P>
-    <p>총 사망자 : {lst[6]}명</p>
-    <p>오늘 사망자 : {lst[7]}명</p>
-
+    <p>Total recovered : {lst[4]}</p>
+    <p>Daily recovered : {lst[5]}</P>
+    <p>Total deaths : {lst[6]}</p>
+    <p>Daily deaths : {lst[7]}</p>
 </body>
-
+</html>
     '''.format(**locals()),subtype = 'html')
-
 
 total_cases = lst[0]
 
-def check_data():
+def check_data(): # check if total cases changed 
     if lst[0] != total_cases:
         send_mail()
 
 def send_mail():
     with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
         smtp.login(EMAIL_ADD,EMAIL_PASS)
-
         smtp.send_message(msg)
 
 while True:  
     check_data()
-    time.sleep(42000)
+    time.sleep(42000) # set time ( seconds ) to time desired. 
 
 
